7ea5e21af69d36f4c948da32ab38213301c5c88d
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-09-25 22:35:29 +0100

    mini project complete

--- miniProject/calcul.c
+++ miniProject/calcul.c
@@ -1,57 +1,169 @@
 #include <stdio.h>
-void addition(){
-int n , i ,somme = 0 , num;
-printf("entrer le nombre des valeurs voulez vous : ");
-scanf("%d",&n);
-for(i=0; i<n;i++){
-printf("entrer un nombre ");
-scanf("%d",&num);
-somme += num ;
-}
-printf("%d\n",somme);
-}
-int main(){
-    int a , b , operation , somme , abstraction , multiplication,divison;
-    do{
-        printf("choix 1 : ajouter\n");
-        printf("choix 2 : Soustraction\n");
-        printf("choix 3 : Multiplication\n");
-        printf("choix 4 : Division\n");
-        printf("choix 5 : Moyenne\n");
-        printf("choix 6 : Valeur absolue\n");
-        printf("choix 7 : Exponentiation\n");
-        printf("choix 8 : Racine carrée\n");
-        printf("choix 9 : quitter\n");
-        scanf("%d",&operation);
+#include <stdlib.h>
+#include <math.h>
 
-        switch(operation){
-            case 1 :
+void addition()
+{
+    int n, i, somme = 0, num;
+    printf("Entrez le nombre de valeurs que vous voulez additionner : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Entrez un nombre : ");
+        scanf("%d", &num);
+        somme += num;
+    }
+
+    printf("La somme des valeurs est : %d\n", somme);
+}
+
+void soustraction()
+{
+    int a, b, result;
+
+    printf("Entrez un nombre : ");
+    scanf("%d", &a);
+
+    printf("entrer un nombre : ");
+    scanf("%d", &b);
+
+    result = a - b;
+
+    printf("Le resultat de la soustraction est : %d\n", result);
+}
+void multiplication()
+{
+
+    int a, b, result;
+
+    printf("Entrez un nombre : ");
+    scanf("%d", &a);
+
+    printf("entrer un nombre : ");
+    scanf("%d", &b);
+
+    result = a * b;
+
+    printf("Le resultat de la multiplication est : %d\n", result);
+}
+int division()
+{
+    int a, b, result;
+
+    printf("Entrez un nombre : ");
+    scanf("%d", &a);
+
+    printf("entrer un nombre : ");
+    scanf("%d", &b);
+
+    if (b != 0)
+    {
+        result = a / b;
+    }
+    else
+    {
+        printf("error :(");
+        return 1;
+    }
+
+    printf("Le resultat de la division est : %d\n", result);
+}
+void moyenne(){
+    int n, i, somme = 0, num , moy;
+    printf("Entrez le nombre de valeurs que vous voulez additionner : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Entrez un nombre : ");
+        scanf("%d", &num);
+        somme += num;
+    }
+    
+    moy = somme / n;
+
+    printf("La moyenne des valeurs est : %d\n", moy);
+}
+void absolute(){
+    int value , absolu;
+    printf("entrer un nombre : ");
+    scanf("%d",&value);
+    absolu = abs(value);
+    printf("la result de la valeur absolu et : %d ", absolu);
+}
+void exponentation(){
+    int base , expon , result ;
+    printf("entrer la base ");
+    scanf("%d",&base);
+    printf("entrer l exposent ");
+    scanf("%d",&expon);
+
+    result = pow(base,expon);
+    printf("la result de l exponentation et %d",result);
+
+}
+
+void racine(){
+    int n , result ;
+    printf("entrer un nombre ");
+    scanf("%d",&n);
+    result = sqrt(n);
+    printf("la result de racine et %d",result);
+}
+int main()
+{
+    int choix;
+
+    do
+    {
+        printf("\nChoix du menu :\n");
+        printf("1 : Addition\n");
+        printf("2 : Soustraction\n");
+        printf("3 : Multiplication\n");
+        printf("4 : Division\n");
+        printf("5 : moyenne\n");
+        printf("6 : absolu\n");
+        printf("7 : exponentation\n");
+        printf("8 : racine\n");
+        printf("9 : Quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix)
+        {
+        case 1:
             addition();
-            case 2 : 
-            printf("entrer nombre ");
-            scanf("%d",&a);
-            printf("entre nombre ");
-            scanf("%d",&b);
-            abstraction = a-b;
-            printf("%d",abstraction);
             break;
-            case 3 : 
-            multiplication = a * b ;
-            printf("%d",multiplication);
+        case 2:
+            soustraction();
             break;
-            case 4 : 
-            if(b == 0){
-                printf("erreur ");
-                return 1;
-            }
-            divison = a / b ; 
-            printf("%d",divison);
+        case 3:
+            multiplication();
             break;
-            default:
-            printf("veuillez saisir un nombre dans les choix donne on top ");
+        case 4:
+            division();
+            break;
+        case 5:
+            moyenne();
+            break;
+        case 6:
+            absolute();
+            break;
+        case 7:
+            exponentation();
+            break;
+        case 8:
+            racine();
+            break;
+        case 9:
+            printf("Au revoir !\n");
+            break;
+        default:
+            printf("Choix invalide, essayez encore.\n");
             break;
         }
-    } while (operation != 9);
+    } while (choix != 9);
+
     return 0;
-    
 }
