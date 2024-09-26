b0e5c04556c383c7fd71965130828384666116fb
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-09-26 22:33:27 +0100

    ..

--- miniProject/gestionLivre.c
+++ miniProject/gestionLivre.c
@@ -3,7 +3,7 @@ typedef struct
 {
     char titre[100];
     char auteur[100];
-    float prix;
+    int prix;
     int quantity;
 } livres;
 
@@ -12,19 +12,47 @@ int count = 0;
 
 void ajouter()
 {
-    printf("entrer le titre de livre");
+    printf("entrer le titre de livre : ");
     scanf("%s", livre[count].titre);
-    printf("entrer l auteur de livre ");
+    printf("entrer l auteur de livre : ");
     scanf("%s", livre[count].auteur);
-    printf("entrer le prix de livre ");
-    scanf("%f", livre[count].prix);
-    printf("entrer le quantity de livre ");
-    scanf("%d", livre[count].quantity);
+    printf("entrer le prix de livre : ");
+    scanf("%d", &livre[count].prix);
+    printf("entrer le quantity de livre : ");
+    scanf("%d", &livre[count].quantity);
     count++;
-    printf("le livre et ajouter successfully");
+    printf("le livre et ajouter successfully \n");
+}
+void afficher(){
+    for(int i = 0;i<count;i++){
+        printf("--- livre %d --- \n",count);
+        printf("titre : %s , autheur : %s , price : %d , quantity : %d \n",livre[i].titre,livre[i].auteur,livre[i].prix,livre[i].quantity);
+        printf("---------------\n");
+    }
+}
+void modifier(){
+    char title[50];
+    printf("enter le titre de ce livre ");
+    scanf("%s",title);
+    int found = 0 ;
+    for(int i =0;i < count;i++){
+        if(livre[i].titre == title){
+            found = 1;
+            printf("entrer le quantity de livre : ");
+            scanf("%d", &livre[i].quantity);
+            printf("livre %s is modified successfuly \n", livre[i].titre);
+        }
+    }
+    if(found == 0){
+        printf("livre not found \n");
+    }
+
 }
 int main()
 {
     ajouter();
+    modifier();
+    afficher();
+    printf("%d", count);
     return 0;
 }
