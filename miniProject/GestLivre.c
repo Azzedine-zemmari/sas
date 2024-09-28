31918733da5ca09865a42014a327e68b5de32faf
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-09-28 09:26:41 +0100

    fix suppersion

--- miniProject/GestLivre.c
+++ miniProject/GestLivre.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
+#define RESET "\033[0m"
+#define BLUE "\033[34m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
 int main()
 {
-    char titre[100][20];
+    char titre[100][40];
     char auteur[100][40];
     float prix[100];
     int quantity[100];
@@ -16,30 +19,29 @@ int main()
     do
     {
         printf("\nChoix du menu :\n");
-        printf("1 : ajouter\n");
-        printf("2 : afficher tous\n");
-        printf("3 : modifier quantity de livre\n");
-        printf("4 : recherche de livre\n");
-        printf("5 : afficher nombre total des livre\n");
-        printf("6 : supprimer un livre\n");
-        printf("7 : Quitter\n");
+        printf(BLUE "1 : ajouter\n");
+        printf(BLUE "2 : afficher tous\n");
+        printf(BLUE "3 : modifier quantity de livre\n");
+        printf(BLUE "4 : recherche de livre\n");
+        printf(BLUE "5 : afficher nombre total des livre\n");
+        printf(BLUE "6 : supprimer un livre\n");
+        printf(BLUE "7 : Quitter\n" RESET);
         printf("Votre choix : ");
         scanf("%d", &choix);
-        getchar();
         switch (choix)
         {
         case 1:
             do
             {
                 printf("enter le titre :  ");
-                fgets(titre[count],sizeof(titre[count]),stdin);
+                scanf("%s", titre[count]);
                 printf("enter l auteur : ");
                 scanf("%s", auteur[count]);
                 printf("entre le prix ");
                 scanf("%f", &prix[count]);
                 printf("enter la qantity : ");
                 scanf("%d", &quantity[count]);
-                printf("titre %s par success \n", titre[count]);
+                printf(GREEN "titre %s par success \n"RESET, titre[count]);
 
                 count++;
 
@@ -65,12 +67,12 @@ int main()
                     found = 1;
                     printf("entrer la nouvelle quantité : ");
                     scanf("%d", &quantity[i]);
-                    printf("--- la quantité est modifiée avec succès ---\n");
+                    printf(GREEN"--- la quantité est modifiée avec succès ---\n"RESET);
                 }
             }
             if (!found)
             {
-                printf("livre non trouvé \n");
+                printf(RED"livre non trouvé \n"RESET);
             }
             break;
         case 4:
@@ -88,7 +90,7 @@ int main()
             }
             break;
         case 5:
-            printf("le nombre total des livre et %d\n", count);
+            printf(GREEN"le nombre total des livre et %d\n"RESET, count);
             break;
         case 6:
             char rechS[20];
@@ -100,23 +102,24 @@ int main()
                 if (strcmp(titre[i], rechS) == 0)
                 {
                     foundetS = 1;
-                    for(int j=i;j<count-1;i--){
-                        strcpy(titre[j],titre[j+1]);
-                        strcpy(auteur[j],auteur[j+1]);
-                        quantity[j]=quantity[j+1];
-                        prix[j]=prix[j+1];
+                    for (int j = i; j < count - 1;j++)
+                    {
+                        strcpy(titre[j], titre[j + 1]);
+                        strcpy(auteur[j], auteur[j + 1]);
+                        quantity[j] = quantity[j + 1];
+                        prix[j] = prix[j + 1];
                     }
                     count--;
-                }}
+                }
+            }
             break;
         case 7:
-            printf("Au revoir !\n");
+            printf(BLUE"Au revoir !\n"RESET);
             break;
         default:
-            printf("Choix invalide, essayez encore.\n");
+            printf(RED"Choix invalide, essayez encore.\n"RESET);
             break;
         }
     } while (choix != 7);
     return 0;
 }
-
